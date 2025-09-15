526d7413a42425472375ab48f03dbf1d351b79ed
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-15 05:19:14 -0400

    Added Fallback contract deployment

--- ignition/modules/ethernaut.ts
+++ ignition/modules/ethernaut.ts
@@ -2,6 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("CounterModule", (m) => {
   const counter = m.contract("Counter");
+  const fallback = m.contract("Fallback");
 
   m.call(counter, "incBy", [5n]);
 
