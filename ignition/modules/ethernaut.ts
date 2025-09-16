61b9360fc0f025a7f4e2885906841de6a2bba9ee
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-16 00:21:14 -0400

    Added 05 challenge contract Token

--- ignition/modules/ethernaut.ts
+++ ignition/modules/ethernaut.ts
@@ -7,12 +7,14 @@ export default buildModule("Ethernaut", (m) => {
   const fallout = m.contract("Fallout");
   const coinFlip = m.contract("CoinFlip");
   const telephone = m.contract("Telephone");
+  const token = m.contract("Token");
   
   return { 
       fallback,
       fallout,
       coinFlip,
-      telephone
+      telephone,
+      token
   };
 
 });
