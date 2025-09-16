c9532493096661b5d626d5b991b9706ccd6362d2
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-16 04:41:41 -0400

    Commented out 06

--- ignition/modules/ethernaut.ts
+++ ignition/modules/ethernaut.ts
@@ -3,18 +3,25 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("Ethernaut", (m) => {
 
+  console.log(m.getAccount(0));
+
   const fallback = m.contract("Fallback");
   const fallout = m.contract("Fallout");
   const coinFlip = m.contract("CoinFlip");
   const telephone = m.contract("Telephone");
   const token = m.contract("Token", [20n]);
+  // const delegate = m.contract("Delegate");
+  // const delegate = m.deploy(delegateContract, [deployer.address]);
+  // const delegation = m.contract("Delegation");
+  // const delegation = m.deploy(delegationContract, [delegate.address])
   
   return { 
-      fallback,
-      fallout,
-      coinFlip,
-      telephone,
-      token
+    fallback,
+    fallout,
+    coinFlip,
+    telephone,
+    // delegate
+    // delegation
   };
 
 });
