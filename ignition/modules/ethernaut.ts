a7ab695ce2634de752453f18cdd2897b09fbd662
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-16 00:16:07 -0400

    Added 04 challenge contract Telephone

--- ignition/modules/ethernaut.ts
+++ ignition/modules/ethernaut.ts
@@ -6,11 +6,13 @@ export default buildModule("Ethernaut", (m) => {
   const fallback = m.contract("Fallback");
   const fallout = m.contract("Fallout");
   const coinFlip = m.contract("CoinFlip");
+  const telephone = m.contract("Telephone");
   
   return { 
       fallback,
       fallout,
-      coinFlip
+      coinFlip,
+      telephone
   };
 
 });
